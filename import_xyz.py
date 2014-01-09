d31da770b90ce358da33665f28c947e67ca827e4
Anna Petrasova <akratoc@ncsu.edu>
2014-01-09 17:31:25 -0500

    remove table by z coordinate

--- import_xyz.py
+++ import_xyz.py
@@ -12,68 +12,90 @@ import os
 import sys
 import atexit
 import math
+import numpy as np
 from tempfile import mkstemp
 
 from grass.script import core as gcore
 from grass.script import raster as grast
 
-from scan_process import read_from_ascii, difference, adjust_scanned_elev, \
-  adjust_boundaries, remove_fuzzy_edges, calibrate, smooth
+from scan_process import read_from_ascii, difference, remove_fuzzy_edges, smooth, calibrate_points, remove_table, scale_z_exag
 
 
-def import_scan_rinxyz(input_file, real_elev, output_elev, output_diff, mm_resolution, calib_raster=None):
+def import_scan_rinxyz(input_file, real_elev, output_elev, output_diff, mm_resolution, calib_matrix, table_mm):
     output_tmp1 = "output_scan_tmp1"
-    output_tmp2 = "output_scan_tmp2"
-    output_tmp3 = "output_scan_tmp3"
-    mm_resolution *= 1000
+#    output_tmp2 = "output_scan_tmp2"
+#    output_tmp3 = "output_scan_tmp3"
+#    mm_resolution *= 1000
 
     fd, temp_path = mkstemp()
     os.close(fd)
     os.remove(temp_path)
     read_from_ascii(input_file=input_file, output_file=temp_path)
-    gcore.use_temp_region()
-    
-    x_min, x_max, y_min, y_max = remove_fuzzy_edges(input_file=temp_path, output_file=temp_path, resolution=mm_resolution)
 
+    fh = open(temp_path, 'r')
+    array = np.array([map(float, line.split()) for line in fh.readlines()])
+    fh.close()
+
+    # calibrate points by given matrix
+    array = calibrate_points(array, calib_matrix).T
+
+    # remove underlying table   
+    array = remove_table(array, table_mm)
+
+    # remove fuzzy edges
+    array, x_min, x_max, y_min, y_max = remove_fuzzy_edges(array, resolution=mm_resolution)
+
+    # scale Z to original and apply exaggeration
+    raster_info = grast.raster_info(real_elev)
+    array = scale_z_exag(array, raster_info, 3.5)
+    
+    # save resulting array
+    np.savetxt(temp_path, array, delimiter=" ")
+
+    # import
     gcore.run_command('g.region', n=y_max, s=y_min, e=x_max, w=x_min, res=mm_resolution)
     gcore.run_command('r.in.xyz', separator=" ", input=temp_path, output=output_tmp1, overwrite=True)
+    os.remove(temp_path)
 
     info = grast.raster_info(output_tmp1)
     if  math.isnan(info['min']) or math.isnan(info['max']):
         gcore.run_command('g.remove', rast=output_tmp1)
-        return
-
-    
-
+        return    
 
     gcore.run_command('r.region', map=output_tmp1, raster=real_elev, align=real_elev)
     gcore.run_command('g.region', rast=output_tmp1)
 
-    if calib_raster:
-        calibrate(scanned_elev=output_tmp1, calib_elev=calib_raster, new=output_tmp2)
-        adjust_scanned_elev(real_elev=real_elev, scanned_elev=output_tmp2, new=output_tmp3)
-        smooth(scanned_elev=output_tmp3, new=output_elev)
-        gcore.run_command('g.remove', rast=output_tmp3)
-    else:
-        adjust_scanned_elev(real_elev=real_elev, scanned_elev=output_tmp1, new=output_tmp2)
-        smooth(scanned_elev=output_tmp2, new=output_elev)
+#    if calib_raster:
+#        calibrate(scanned_elev=output_tmp1, calib_elev=calib_raster, new=output_tmp2)
+#        adjust_scanned_elev(real_elev=real_elev, scanned_elev=output_tmp2, new=output_tmp3)
+#        smooth(scanned_elev=output_tmp3, new=output_elev)
+#        gcore.run_command('g.remove', rast=output_tmp3)
+#    else:
+#    adjust_scanned_elev(real_elev=real_elev, scanned_elev=output_tmp1, new=output_tmp2)
+    smooth(scanned_elev=output_tmp1, new=output_elev)
+    gcore.run_command('r.colors', map=output_elev, color='elevation')
         
 
     difference(real_elev=real_elev, scanned_elev=output_elev, new=output_diff)
 
     gcore.run_command('g.remove', rast=output_tmp1)
-    gcore.run_command('g.remove', rast=output_tmp2)
+#    gcore.run_command('g.remove', rast=output_tmp2)
 
 
 
 def main():
+    import subprocess
     gcore.use_temp_region()
-    import_scan_rinxyz(input_file=r"C:\Users\akratoc\TanGeoMS\output\scan.txt",
-                       real_elev='elevation',
+    mesh_path = r"C:\Users\akratoc\TanGeoMS\output\scan.txt"
+    subprocess.call([r"C:\Users\akratoc\TanGeoMS\Basic\new4\KinectFusionBasics-D2D\Debug\KinectFusionBasics-D2D.exe", mesh_path, '5', str(0.4), str(0.75)])
+    calib_matrix = np.load(r"C:\Users\akratoc\TanGeoMS\output\calib_matrix.npy")
+    import_scan_rinxyz(input_file=mesh_path,
+                       real_elev='elevation@user1',
                        output_elev='scan',
                        output_diff='diff',
-                       mm_resolution=0.002,
-                       calib_raster=None)
+                       mm_resolution=0.001,
+                       calib_matrix=calib_matrix,
+                       table_mm=4)
 
 def cleanup():
     gcore.del_temp_region()
--- scan_process.py
+++ scan_process.py
@@ -38,7 +38,7 @@ def get_calibration_matrix(array):
 
 
 def calibrate_points(array, calib_matrix):
-    return np.dot(calib_matrix, array)
+    return np.dot(calib_matrix, array.T)
 
 
 def unit_vector(vector):
@@ -66,18 +66,68 @@ def angle_between(v1, v2):
             return np.pi
     return angle
 
+def remove_table(array, height):
+    """Removes points under the model (table).
+    height is in mm.    
+    """
+    z = array[:, 2] * 1000
+    min_z = np.min(z)
+    max_z = np.max(z)
+    range_mm = int(max_z - min_z)
+    hist_values, bin_edges = np.histogram(z, bins=range_mm)
+    max_idx = np.argmax(hist_values)
+    minimas = (np.diff(np.sign(np.diff(smooth_signal(hist_values, window_len=5)))) > 0).nonzero()[0] + 1
+    
+#    from pylab import plot, show
+#    plot(list(smooth_signal(hist_values, window_len=3)))
+#    show()
+   
+    min_z = np.min(z)
+    min_z += minimas[minimas > max_idx][0] + height
+    array = array[z >= min_z]
+    return array
+    
+    
+def scale_z_exag(array, raster_info, zexag):
+    """Change z range to match z-exag"""
+    old_min_z = np.min(array[:, 2])
+#    old_max_z = np.max(array[:, 2])
+    old_min_x = np.min(array[:, 0])
+    old_max_x = np.max(array[:, 0])
+    old_min_y = np.min(array[:, 1])
+    old_max_y = np.max(array[:, 1])
+    
+    ns_scale = (raster_info['north'] - raster_info['south']) / (old_max_y - old_min_y)
+    ew_scale = (raster_info['east'] - raster_info['west'] ) / (old_max_x - old_min_x)
+    hor_scale = (ns_scale + ew_scale) / 2
+    scale = float(hor_scale) / zexag
+    print "SCALE: " + str(hor_scale)
+    print "Z-EXAGGERATION: " + str(zexag)
+    print "1 cm in height ~ {} m".format(0.01 * scale)
+    array[:, 2] = array[:, 2] * scale + raster_info['min'] - old_min_z * scale
+    return array
 
-def remove_fuzzy_edges(array, resolution, tolerance=0.8):
+
+def scale_z_raster(array, raster_info):
+    """Change z range to match raster map range"""
+    old_min_z = np.min(array[:, 2])
+    old_max_z = np.max(array[:, 2])    
+    old_range = old_max_z - old_min_z
+    new_range = raster_info['max'] - raster_info['min']
+    array[:, 2] = (((array[:, 2] - old_min_z) * new_range) / old_range) + raster_info['min']
+    return array
+        
+
+def remove_fuzzy_edges(array, resolution, tolerance=0.5):
     bins_n = (np.max(array[:, 0]) - np.min(array[:, 0])) / resolution
     H, yedges, xedges = np.histogram2d(-array[:, 1], array[:, 0], bins=(bins_n, bins_n))
-
+    
 #    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 #    import matplotlib.pyplot as plt
 #    np.clip(H, 0, 1, H)
 #    plt.ion()
 #    plt.imshow(H, extent=extent, interpolation='nearest', origin='upper')
 #    plt.colorbar()
-#    
 #    plt.show()
 
 
@@ -110,10 +160,10 @@ def remove_fuzzy_edges(array, resolution, tolerance=0.8):
 
     limit_x_w = xedges[col]
 
-#    array = array[array[:, 0] <= limit_x_e]
-#    array = array[array[:, 0] >= limit_x_w]
-#    array = array[array[:, 1] <= -limit_y_s]
-#    array = array[array[:, 1] >= -limit_y_n]
+    array = array[array[:, 0] <= limit_x_e]
+    array = array[array[:, 0] >= limit_x_w]
+    array = array[array[:, 1] <= -limit_y_s]
+    array = array[array[:, 1] >= -limit_y_n]
 #    bins_n = (np.max(array[:, 0]) - np.min(array[:, 0])) / resolution
 #    H, yedges, xedges = np.histogram2d(-array[:, 1], array[:, 0], bins=(bins_n, bins_n))
 #    np.clip(H, 0, 1, H)
@@ -125,9 +175,66 @@ def remove_fuzzy_edges(array, resolution, tolerance=0.8):
     #np.savetxt(output_file, array, delimiter=" ")
 #    print limit_x_w, limit_x_e, limit_y_s, limit_y_n
 #    print np.min(array[:, 0]), np.max(array[:, 0]), np.min(array[:, 1]), np.max(array[:, 1])
-    return limit_x_w, limit_x_e, -limit_y_n, -limit_y_s
+    return array, limit_x_w, limit_x_e, -limit_y_n, -limit_y_s
 #    return np.min(array[:, 0]), np.max(array[:, 0]), np.min(array[:, 1]), np.max(array[:, 1])
 
+
+def smooth_signal(x, window_len=11, window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y
 
 ###################### GRASS ####################################
 
@@ -142,20 +249,22 @@ def adjust_scanned_elev(real_elev, scanned_elev, new):
     
     expression = "{new} = {a} + {b} * {elev}".format(new=new, a=res['a'], b=res['b'], elev=scanned_elev)
     grast.mapcalc(expression, overwrite=True)
+    gcore.run_command('r.colors', map=new, color='elevation')
 
 def difference(real_elev, scanned_elev, new):
     info = grast.raster_info(real_elev)
 #    expression = "{new} = ({real_elev} - {scanned_elev}) / ({max} - {min}) * 100".format(new=new, real_elev=real_elev,
-    expression = "{new} = {real_elev} - {scanned_elev}".format(new=new, real_elev=real_elev,
+    expression = "{new} = {scanned_elev} - {real_elev}".format(new=new, real_elev=real_elev,
                  scanned_elev=scanned_elev, max=info['max'], min=info['min'])
     grast.mapcalc(expression, overwrite=True)
-    gcore.run_command('r.colors', map=new, color='differences', flags='n')
+    gcore.run_command('r.colors', map=new, color='differences')
 
 
 def read_from_obj(input_file, output_file, rotate_180=True):
     fh_input = open(input_file, 'r')
     fh_output = open(output_file, 'w')
 
+    scale = 1
     count = 0
     for line in fh_input.readlines():
         count += 1
@@ -165,9 +274,9 @@ def read_from_obj(input_file, output_file, rotate_180=True):
                 line_list.remove("v") # remove the string "v"
                 x, y, z = map(float, line_list)
                 if rotate_180:
-                    fh_output.write("{x} {y} {z}\n".format(x=-x * 1000, y=-y * 1000, z=z * 1000))
+                    fh_output.write("{x} {y} {z}\n".format(x=-x * scale, y=-y * scale, z=z * scale))
                 else:
-                    fh_output.write("{x} {y} {z}\n".format(x=x * 1000, y=y * 1000, z=z * 1000))
+                    fh_output.write("{x} {y} {z}\n".format(x=x * scale, y=y * scale, z=z * scale))
 
     fh_input.close()
     fh_output.close()
@@ -180,14 +289,15 @@ def read_from_ascii(input_file, output_file, rotate_180=True):
     fh_input = open(input_file, 'r')
     fh_output = open(output_file, 'w')
 
+    scale = 1
     count = 0
     for line in fh_input.readlines():
         count += 1
         x, y, z = map(float, line.split())
         if rotate_180:
-            fh_output.write("{x} {y} {z}\n".format(x=-x * 1000, y=-y * 1000, z=z * 1000))
+            fh_output.write("{x} {y} {z}\n".format(x=-x * scale, y=-y * scale, z=z * scale))
         else:
-            fh_output.write("{x} {y} {z}\n".format(x=x * 1000, y=y * 1000, z=z * 1000))
+            fh_output.write("{x} {y} {z}\n".format(x=x * scale, y=y * scale, z=z * scale))
 
     fh_input.close()
     fh_output.close()
@@ -200,6 +310,7 @@ def calibrate(scanned_elev, calib_elev, new):
     grast.mapcalc(expression, overwrite=True)
 
 def smooth(scanned_elev, new):
-    gcore.run_command('r.neighbors', input=scanned_elev, output=new, size=5, overwrite=True)
+    gcore.run_command('r.neighbors', input=scanned_elev, output=new, size=9, overwrite=True)
 
-
+def flowacc(scanned_elev, new):
+    gcore.run_command('r.flow', elevation=scanned_elev, flowaccumulation=new, overwrite=True)
