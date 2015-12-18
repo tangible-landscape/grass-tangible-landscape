61649641801079e2829524b8e1678817b68088fb
Anna Petrasova <kratochanna@gmail.com>
2015-12-18 16:25:12 -0500

    scan_processing not necessary anymore

---
+++ utils.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+@brief utils
+
+This program is free software under the GNU General Public License
+(>=v2). Read the file COPYING that comes with GRASS for details.
+
+@author: Anna Petrasova (akratoc@ncsu.edu)
+"""
+import os
+import uuid
+
+from grass.script import core as gcore
+
+def get_environment(tmp_regions, **kwargs):
+    """!Returns environment for running modules.
+    All modules for which region is important should
+    pass this environment into run_command or similar.
+
+    @param tmp_regions a list of temporary regions
+    @param kwargs arguments for g.region
+
+    @return environment as a dictionary
+    """
+    name = str(uuid.uuid4())[:8]
+    gcore.run_command('g.region', flags='u', save=name, **kwargs)
+    tmp_regions.append(name)
+    env = os.environ.copy()
+    env['WIND_OVERRIDE'] = name
+    env['GRASS_OVERWRITE'] = '1'
+    env['GRASS_VERBOSE'] = '0'
+    env['GRASS_MESSAGE_FORMAT'] = 'standard'
+    if 'GRASS_REGION' in env:
+        del env['GRASS_REGION']
+    return env
+
+
+def remove_temp_regions(regions):
+    """!Removes temporary regions."""
+    gisenv = gcore.gisenv()
+    path_to_regions = os.path.join(gisenv['GISDBASE'], gisenv['LOCATION_NAME'], gisenv['MAPSET'], 'windows')
+    for region in regions:
+        os.remove(os.path.join(path_to_regions, region))
