620c925988b909a6bc666c1957f6e75c0c5aabfd
Anna Petrasova <kratochanna@gmail.com>
2015-12-18 15:58:28 -0500

    plugin: add tab and remove text widget

--- tangeoms_plugin.py
+++ tangeoms_plugin.py
@@ -8,7 +8,7 @@ Created on Wed Nov 20 14:44:32 2013
 import wx
 import os
 import sys
-from tempfile import gettempdir
+
 from watchdog.observers import Observer
 from change_handler import RasterChangeHandler
 
@@ -24,157 +24,26 @@ from run_analyses import run_analyses
 updateGUIEvt, EVT_UPDATE_GUI = wx.lib.newevent.NewCommandEvent()
 
 
-class TangeomsPlugin(wx.Dialog):
+class TangibleLandscapePlugin(wx.Dialog):
     def __init__(self, giface, parent):
         wx.Dialog.__init__(self, parent, title="Tangible Landscape", style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER)
         self.giface=giface
         self.parent=parent
-        mainSizer = wx.BoxSizer(wx.VERTICAL)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        modelBox = wx.StaticBox(self, label="Scanning parameters")
-        modelSizer = wx.StaticBoxSizer(modelBox, wx.VERTICAL)
 
-        # create widgets
-        btnCalibrate = wx.Button(self, label="Calibrate")
-        btnStart = wx.Button(self, label="Start")
-        btnStop = wx.Button(self, label="Stop")
-        btnClose = wx.Button(self, label="Close")
-        btnScanOnce = wx.Button(self, label="Scan once")
-        self.scan_name = wx.TextCtrl(self, value='scan')
-        self.status = wx.StaticText(self)
-        self.textInfo = wx.TextCtrl(self, size=(-1, 100), style=wx.TE_MULTILINE | wx.TE_READONLY)
-        # widgets for model
-        self.elevInput = Select(self, size=(-1, -1), type='raster')
-        self.regionInput = Select(self, size=(-1, -1), type='region')
-        self.zexag = wx.TextCtrl(self)
-        self.rotate = wx.SpinCtrl(self, min=0, max=360, initial=180)
-        self.numscans = wx.SpinCtrl(self, min=1, max=5, initial=1)
-        self.trim = {}
-        for each in 'nsewtb':
-            self.trim[each] = wx.TextCtrl(self, size=(35, -1))
-        self.interpolate = wx.CheckBox(self, label="Use interpolation instead of binning")
-        self.smooth = wx.TextCtrl(self)
-        self.resolution = wx.TextCtrl(self)
-
-        # layout
-        hSizer.Add(btnStart, flag=wx.EXPAND | wx.ALL, border=5)
-        hSizer.Add(btnStop, flag=wx.EXPAND | wx.ALL, border=5)
-        hSizer.AddStretchSpacer()
-        hSizer.Add(btnScanOnce, flag=wx.EXPAND | wx.ALL, border=5)
-        mainSizer.Add(hSizer, flag=wx.EXPAND)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Name of scanned raster:"), flag=wx.EXPAND | wx.ALL | wx.ALIGN_CENTER_VERTICAL, border=5)
-        hSizer.Add(self.scan_name, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
-        mainSizer.Add(hSizer, flag=wx.EXPAND)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(self.status, flag=wx.EXPAND | wx.ALL, border=5)
-        mainSizer.Add(hSizer)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(self.textInfo, flag=wx.EXPAND | wx.ALL, proportion=1, border=5)
-        mainSizer.Add(hSizer, proportion=1, flag=wx.EXPAND)
-        # model parameters
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Reference DEM:"), flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        hSizer.Add(self.elevInput, proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        # region
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Reference region:"), flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        hSizer.Add(self.regionInput, proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Z-exaggeration:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        hSizer.Add(self.zexag, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        # number of scans
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Number of scans:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        hSizer.Add(self.numscans, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Rotation angle:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        hSizer.Add(self.rotate, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        # smooth
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Smooth value:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        hSizer.Add(self.smooth, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        # resolution
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Resolution [mm]:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        hSizer.Add(self.resolution, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Trim scan N, S, E, W [cm]:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        for each in 'nsew':
-            hSizer.Add(self.trim[each], flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(wx.StaticText(self, label="Limit scan vertically T, B [cm]:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        for each in 'tb':
-            hSizer.Add(self.trim[each], flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(self.interpolate, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
-        modelSizer.Add(hSizer, flag=wx.EXPAND)
-        mainSizer.Add(modelSizer, flag=wx.EXPAND|wx.ALL, border=5)
-
-        hSizer = wx.BoxSizer(wx.HORIZONTAL)
-        hSizer.Add(btnCalibrate, flag=wx.EXPAND | wx.ALL, border=5)
-        hSizer.AddStretchSpacer()
-        hSizer.Add(btnClose, flag=wx.EXPAND | wx.ALL, border=5)
-        mainSizer.Add(hSizer, flag=wx.EXPAND)
-
-        # bind events
-        btnStart.Bind(wx.EVT_BUTTON, lambda evt: self.Start())
-        btnStop.Bind(wx.EVT_BUTTON, lambda evt: self.Stop())
-        btnClose.Bind(wx.EVT_BUTTON, self.OnClose)
-        btnCalibrate.Bind(wx.EVT_BUTTON, self.Calibrate)
-        btnScanOnce.Bind(wx.EVT_BUTTON, self.ScanOnce)
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
-        self.Bind(EVT_UPDATE_GUI, self.OnUpdate)
-
-        self.SetSizer(mainSizer)
-        mainSizer.Fit(self)
-
-    def OnClose(self, event):
-        self.Stop()
-        self.Destroy()
-
-    def Start(self):
-        raise NotImplementedError
-
-    def Stop(self):
-        raise NotImplementedError
-
-    def OnUpdate(self, event):
-        for each in self.giface.GetAllMapDisplays():
-            each.GetMapWindow().UpdateMap()
-        self.UpdateText()
-
-    def Calibrate(self, event):
-        from prepare_calibration import write_matrix
-        matrix_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'calib_matrix.txt')
-        write_matrix(matrix_path=matrix_file_path)
-        # update
-        with open(matrix_file_path, 'r') as f:
-            self.calib_matrix = f.read()
-
-
-class TangeomsImportPlugin(TangeomsPlugin):
-    def __init__(self, giface, guiparent,  elev_real, scan, scanFile, minZ, maxZ):
-        TangeomsPlugin.__init__(self, giface, guiparent)
-        self.output = scan
-        self.tmp_file = scanFile
-        self.minZ = minZ
-        self.maxZ = maxZ
+        self.output = 'scan'
         self.data = {'scan_name': self.output, 'info_text': [],
-                     'elevation': elev_real, 'region': '',
+                     'elevation': '', 'region': '',
                      'zexag': 1., 'smooth': 7, 'numscans': 1,
                      'rotation_angle': 180, 'resolution': 2,
                      'trim_nsewtb': [0, 0, 0, 0, 60, 100],
                      'interpolate': False}
+                     
+        self.notebook = wx.Notebook(self)
+        scanning_panel = wx.Panel(self.notebook)
+        self.notebook.AddPage(scanning_panel, "Scanning")
+        self.layout(scanning_panel)
+        self.Layout()
+        
         self.elevInput.SetValue(self.data['elevation'])
         self.zexag.SetValue(str(self.data['zexag']))
         self.rotate.SetValue(self.data['rotation_angle'])
@@ -200,6 +69,131 @@ class TangeomsImportPlugin(TangeomsPlugin):
         self.changedInput = False
         self.Bind(wx.EVT_TIMER, self.RestartIfNotRunning, self.timer)
         self.BindModelProperties()
+        
+    def layout(self, panel):
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        # create widgets
+        btnCalibrate = wx.Button(panel, label="Calibrate")
+        btnStart = wx.Button(panel, label="Start")
+        btnStop = wx.Button(panel, label="Stop")
+        btnClose = wx.Button(panel, label="Close")
+        btnScanOnce = wx.Button(panel, label="Scan once")
+        self.scan_name = wx.TextCtrl(panel, value='scan')
+        self.status = wx.StaticText(panel)
+        # widgets for model
+        self.elevInput = Select(panel, size=(-1, -1), type='raster')
+        self.regionInput = Select(panel, size=(-1, -1), type='region')
+        self.zexag = wx.TextCtrl(panel)
+        self.rotate = wx.SpinCtrl(panel, min=0, max=360, initial=180)
+        self.numscans = wx.SpinCtrl(panel, min=1, max=5, initial=1)
+        self.trim = {}
+        for each in 'nsewtb':
+            self.trim[each] = wx.TextCtrl(panel, size=(35, -1))
+        self.interpolate = wx.CheckBox(panel, label="Use interpolation instead of binning")
+        self.smooth = wx.TextCtrl(panel)
+        self.resolution = wx.TextCtrl(panel)
+
+        # layout
+        hSizer.Add(btnStart, flag=wx.EXPAND | wx.ALL, border=5)
+        hSizer.Add(btnStop, flag=wx.EXPAND | wx.ALL, border=5)
+        hSizer.AddStretchSpacer()
+        hSizer.Add(btnScanOnce, flag=wx.EXPAND | wx.ALL, border=5)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Name of scanned raster:"), flag=wx.EXPAND | wx.ALL | wx.ALIGN_CENTER_VERTICAL, border=5)
+        hSizer.Add(self.scan_name, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(self.status, flag=wx.EXPAND | wx.ALL, border=5)
+        mainSizer.Add(hSizer)
+        # model parameters
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Reference DEM:"), flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        hSizer.Add(self.elevInput, proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        # region
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Reference region:"), flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        hSizer.Add(self.regionInput, proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Z-exaggeration:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        hSizer.Add(self.zexag, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        # number of scans
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Number of scans:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        hSizer.Add(self.numscans, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Rotation angle:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        hSizer.Add(self.rotate, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        # smooth
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Smooth value:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        hSizer.Add(self.smooth, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        # resolution
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Resolution [mm]:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        hSizer.Add(self.resolution, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Trim scan N, S, E, W [cm]:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        for each in 'nsew':
+            hSizer.Add(self.trim[each], flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(wx.StaticText(panel, label="Limit scan vertically T, B [cm]:"), proportion=1, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        for each in 'tb':
+            hSizer.Add(self.trim[each], flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(self.interpolate, flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL, border=3)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+
+        hSizer = wx.BoxSizer(wx.HORIZONTAL)
+        hSizer.Add(btnCalibrate, flag=wx.EXPAND | wx.ALL, border=5)
+        hSizer.AddStretchSpacer()
+        hSizer.Add(btnClose, flag=wx.EXPAND | wx.ALL, border=5)
+        mainSizer.Add(hSizer, flag=wx.EXPAND)
+
+        # bind events
+        btnStart.Bind(wx.EVT_BUTTON, lambda evt: self.Start())
+        btnStop.Bind(wx.EVT_BUTTON, lambda evt: self.Stop())
+        btnClose.Bind(wx.EVT_BUTTON, self.OnClose)
+        btnCalibrate.Bind(wx.EVT_BUTTON, self.Calibrate)
+        btnScanOnce.Bind(wx.EVT_BUTTON, self.ScanOnce)
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        self.Bind(EVT_UPDATE_GUI, self.OnUpdate)
+
+        panel.SetSizer(mainSizer)
+        mainSizer.Fit(panel)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.notebook, 1, wx.ALL | wx.EXPAND, 5)
+        self.SetSizer(sizer)
+        self.SetMinSize(self.GetBestSize())
+        self.Layout()
+
+    def OnClose(self, event):
+        self.Stop()
+        self.Destroy()
+
+    def OnUpdate(self, event):
+        for each in self.giface.GetAllMapDisplays():
+            each.GetMapWindow().UpdateMap()
+
+    def Calibrate(self, event):
+        from prepare_calibration import write_matrix
+        matrix_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'calib_matrix.txt')
+        write_matrix(matrix_path=matrix_file_path)
+        # update
+        with open(matrix_file_path, 'r') as f:
+            self.calib_matrix = f.read()
+
 
     def BindModelProperties(self):
         self.scan_name.Bind(wx.EVT_TEXT, self.OnScanName)
@@ -252,7 +246,7 @@ class TangeomsImportPlugin(TangeomsPlugin):
 
     def OnModelProperties(self, event):
         self.data['elevation'] = self.elevInput.GetValue()
-        self.data['region'] = self.elevInput.GetValue()
+        self.data['region'] = self.regionInput.GetValue()
         self.data['rotation_angle'] = self.rotate.GetValue()
         self.data['numscans'] = self.numscans.GetValue()
         self.data['interpolate'] = self.interpolate.IsChecked()
@@ -266,10 +260,6 @@ class TangeomsImportPlugin(TangeomsPlugin):
         except ValueError:
             pass
         self.changedInput = True
-
-    def UpdateText(self):
-        self.textInfo.SetValue(os.linesep.join(self.data['info_text']))
-        del self.data['info_text'][:]
 
     def RestartIfNotRunning(self, event):
         """Mechanism to restart scanning if process ends or
@@ -329,8 +319,7 @@ class TangeomsImportPlugin(TangeomsPlugin):
 
 
 def run(giface, guiparent):
-    dlg = TangeomsImportPlugin(giface, guiparent, elev_real='elevation', scan='scan',
-                scanFile=os.path.join(os.path.realpath(gettempdir()), 'kinect_scan.txt'), minZ=0.4, maxZ=1.2)
+    dlg = TangibleLandscapePlugin(giface, guiparent)
     dlg.CenterOnParent()
     dlg.Show()
 
