ffed4652ef503451113d8c86f0dbbbd06f3315f7
Anna Petrasova <akratoc@ncsu.edu>
2017-03-09 17:31:11 -0500

    finalize drain scoring

--- experiment_drain.py
+++ experiment_drain.py
@@ -15,23 +15,14 @@ from tangible_utils import get_environment
 import grass.script as gscript
 
 
+solution = [(316715, 251545, 1134.5),
+            (317045, 251065, 1166.9),
+            (317485, 252475, 1005.0),
+            (316765, 250475, 1271.5),
+            (317055, 250705, 1189.9),
+            (315725, 251855, 929.2),
+            (317705, 252645, 940.2)]
 
-# needed layers PERMANENT:
-# r elevation
-# v target point
-# v contours
-
-# needed empty layers in scanning mapset:
-# v drain
-# v change
-
-solution = [(316715, 251545),
-            (317045, 251065),
-            (317485, 252475),
-            (316765, 250475),
-            (317055, 250705),
-            (315725, 251855),
-            (317705, 252645)]
 
 def run_drain(real_elev, scanned_elev, eventHandler, env, subTask, **kwargs):
     before = 'scan_saved'
@@ -48,12 +39,14 @@ def run_drain(real_elev, scanned_elev, eventHandler, env, subTask, **kwargs):
         gscript.run_command('v.edit', map=drain, tool='create', env=env)
 
     # copy results
-    postfix = datetime.now().strftime('%H_%M_%S')
-    prefix = 'drain'
-    gscript.run_command('g.copy', vector=['change', '{}_change_{}_{}'.format(prefix, subTask, postfix)], env=env)
+    if point:
+        postfix = datetime.now().strftime('%H_%M_%S')
+        prefix = 'drain'
+        gscript.run_command('g.copy', vector=['change', '{}_change_{}_{}'.format(prefix, subTask, postfix)], env=env)
 
 
 def post_drain(real_elev, scanned_elev, filterResults, timeToFinish, subTask, logDir, env):
+    env2 = get_environment(raster=real_elev)
     gisenv = gscript.gisenv()
     logFile = os.path.join(logDir, 'log_{}_drain.csv'.format(gisenv['LOCATION_NAME']))
     scoreFile = os.path.join(logDir, 'score_{}.csv'.format(gisenv['LOCATION_NAME']))
@@ -64,21 +57,27 @@ def post_drain(real_elev, scanned_elev, filterResults, timeToFinish, subTask, lo
     with open(logFile, mode) as f:
         if subTask == 0:
             f.write('time,distance,subTask\n')
-        distances = []
         for i in range(len(points)):
             px, py, pc = gscript.read_command('v.out.ascii', input=points[i], type='point', format='point', env=env).strip().split('|')
             px, py = float(px), float(py)
             points_text.append('{}|{}'.format(px, py))
             dist = sqrt((px - solution[subTask][0]) * (px - solution[subTask][0]) + (py - solution[subTask][1]) * (py - solution[subTask][1]))
-            distances.append(dist)
+            z = gscript.read_command('r.what', map=real_elev, coordinates=(px, py), env=env2).strip().split('|')[-1]
+            pz = float(z)
+            height_diff = solution[subTask][2] - pz
             time = times[i]
-            f.write('{time},{d},{sub}\n'.format(time='{}:{}:{}'.format(time[0], time[1], time[2]), d=dist, sub=subTask))
-
+            f.write('{time},{d},{hd},{sub}\n'.format(time='{}:{}:{}'.format(time[0], time[1], time[2]), d=dist, hd=height_diff, sub=subTask))
 
     with open(scoreFile, 'a') as f:
-        # mean from last 3 slopes
-        dist_score = sum(distances[-3:]) / 3.
-        # here convert to some scale?
-        f.write("drain distance: {}\n".format(dist_score))
-        f.write("filtered scans: {}\n".format(filterResults))
-        f.write("time: {}\n".format(timeToFinish))
+        # test whether point is inside buffered watershed
+        found = False
+        query = gscript.vector_what(map='drain_basins_buffered', coord=(px, py))
+        for each in query:
+            if each['Category'] == subTask + 1:
+                found = True
+
+        f.write("drain: inside watershed: {}\n".format(found))
+        f.write("drain: horizontal distance: {}\n".format(dist))
+        f.write("drain: vertical distance: {}\n".format(height_diff))
+        f.write("drain: filtered scans: {}\n".format(filterResults))
+        f.write("drain: time since start: {}\n".format(timeToFinish))
